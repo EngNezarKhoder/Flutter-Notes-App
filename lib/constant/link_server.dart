c6c4b2937de028d451f6e9ec1723b6107c3dd87c
EngNezarKhoder <nezarkhoder606@gmail.com>
2025-12-07 20:39:31 +0300

    some editions

--- lib/constant/link_server.dart
+++ lib/constant/link_server.dart
@@ -1,5 +1,5 @@
 const String linkServer = "http://192.168.124.248:8012/Course_Php"; 
-const String linkImageRoute = "http://192.168.188.248:8012/Course_Php/upload";
+const String linkImageRoute = "http://192.168.124.248:8012/Course_Php/upload";
 
 const String signUpLink = "$linkServer/auth/sign_up.php";
 const String loginLink = "$linkServer/auth/login.php";
